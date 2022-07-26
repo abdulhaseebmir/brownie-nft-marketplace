1a8b3bb976c4719d6125414968ebbc19a592af58
abduhaseebmir <mabdulhaseebq@gmail.com>
2022-07-26 13:31:55 +0500

    NftMarketplace listings

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -1,6 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+error NftMarketPlace__PriceMustBeAboveZero();
+error NftMarketPlace__NotApprovedForMarketplace();
+error NftMarketPlace__AlreadyListed(address nftAddress, uint256 tokenId);
+
+
 contract NftMarketPlace {
-    
+
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // NFT Contract address -> NFT -> Listing
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    // Modifier
+    modifier notListed(address nftAddress, uint256 tokenId, address owner) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if(listing.price > 0) {
+            revert NftMarketPlace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    // Main Functions
+    function listItem(address nftAddress, uint256 tokenId, uint256 price) external {
+        if(price <= 0) {
+            revert NftMarketPlace__PriceMustBeAboveZero();
+        }
+        IERC721 nft = IERC721(nftAddress);
+        if(nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketPlace__NotApprovedForMarketplace();
+        }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
 }
