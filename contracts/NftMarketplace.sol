e8de91c23e9f4153077bd965d4494269ba9fd9df
abduhaseebmir <mabdulhaseebq@gmail.com>
2022-07-27 10:49:23 +0500

    getter functions

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -131,4 +131,14 @@ contract NftMarketPlace is ReentrancyGuard {
             revert NftMarketPlace__TransferFailed();
         }
     }
+
+    // Getter Functions
+    function getListing(address nftAddress, uint256 tokenId) 
+    external view returns (Listing memory) {
+        return s_listings[nftAddress][tokenId];
+    }
+
+    function getProceeds(address seller) external view returns(uint256) {
+        return s_proceeds[seller];
+    }
 }
