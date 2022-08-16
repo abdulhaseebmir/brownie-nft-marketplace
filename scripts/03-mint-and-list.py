023f7d14c5927fee692b40b0792da5054edcfec9
abduhaseebmir <mabdulhaseebq@gmail.com>
2022-08-16 11:57:41 +0500

    reuse existing contracts

--- scripts/03-mint-and-list.py
+++ scripts/03-mint-and-list.py
@@ -6,24 +6,28 @@ PRICE = Web3.toWei("0.1", "ether")
 
 def mint_and_list():
     account = get_account()
-    nft_marketplace = NftMarketplace.deploy({
-        "from": account
-    })
-    basic_nft = BasicNft.deploy({
-        "from": account
-    })
+    basic_nft = BasicNft[-1]
+    nft_marketplace = NftMarketplace[-1]
+    
+    # account = get_account()
+    # nft_marketplace = NftMarketplace.deploy({
+    #     "from": account
+    # })
+    # basic_nft = BasicNft.deploy({
+    #     "from": account
+    # })
     print("Minting...")
-    mint_tx_receipt = basic_nft.mintNft()
+    mint_tx_receipt = basic_nft.mintNft({"from": account})
     mint_tx_receipt.wait(1)
     token_id = mint_tx_receipt.events[0]["tokenId"]
     print(f"Basic NFT of token ID: {token_id} minted!")
     
     print("Approving NFT...")
-    approval_tx = basic_nft.approve(nft_marketplace.address, token_id)
+    approval_tx = basic_nft.approve(nft_marketplace.address, token_id, {"from": account})
     approval_tx.wait(1)
 
     print("Listing NFT...")
-    market_place_tx = nft_marketplace.listItem(basic_nft.address, token_id, PRICE)
+    market_place_tx = nft_marketplace.listItem(basic_nft.address, token_id, PRICE, {"from": account})
     market_place_tx.wait(1)
     print("Listed!")
 
